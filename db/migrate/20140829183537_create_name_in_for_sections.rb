0c99862c4a5b4c9d1fa0342b4ff9e92d525289bc
Andrew Robert Dean Akers <andrewrdakers@gmail.com>
2014-08-29 18:37:30 +0000

    it will add a name to sections class

---
+++ db/migrate/20140829183537_create_name_in_for_sections.rb
@@ -0,0 +1,5 @@
+class CreateNameInForSections < ActiveRecord::Migration
+  def change
+    add_column :sections, :name, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140829183212) do
+ActiveRecord::Schema.define(version: 20140829183537) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "sections", force: true do |t|
+    t.string "name"
   end
 
 end
