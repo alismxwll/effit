1dbcba145a42c6b2939f4276ae78cb31cc4b51c2
Andrew Robert Dean Akers <andrewrdakers@gmail.com>
2014-09-01 20:46:27 +0000

    it will add routes for accounts

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,12 @@ Rails.application.routes.draw do
   match('e/:id/edit', {via: :get, to: 'subeffits#edit'})
   match('e/:id', {via: [:patch, :put], to: 'subeffits#update'})
   match('e/:id', {via: :delete, to: 'subeffits#destroy'})
+  
+  match('a', {via: :get, to: 'effers#index'})
+  match('a/new', {via: :get, to: 'effers#new'})
+  match('a/:id', {via: :get, to: 'effers#show'})
+  match('a/', {via: :post, to: 'effers#create'})
+  match('a/:id/edit', {via: :get, to: 'effers#edit'})
+  match('a/:id', {via: [:patch, :put], to: 'effers#update'})
+  match('a/:id', {via: :delete, to: 'effers#destroy'})
 end
