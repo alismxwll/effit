0066a416539dec5c0afee47564fb46401d341d22
Andrew Robert Dean Akers <andrewrdakers@gmail.com>
2014-09-02 00:09:47 +0000

    it will add postit associtations to effer and subeffit

--- spec/models/postit_spec.rb
+++ spec/models/postit_spec.rb
@@ -3,6 +3,6 @@ require 'rails_helper'
 describe Postit do
   it { should validate_presence_of :name }
   it { should validate_presence_of :content }
-  it { should belong_to :effers }
-  it { should belong_to :subeffits }
+  it { should belong_to :effer }
+  it { should belong_to :subeffit }
 end
