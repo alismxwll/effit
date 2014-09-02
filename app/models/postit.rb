023b749daa95e5c246d2f884743e5b4075f758fc
Andrew Robert Dean Akers <andrewrdakers@gmail.com>
2014-09-02 00:21:55 +0000

    it will fix error of having published scope in subeffits instead of postits

--- app/models/postit.rb
+++ app/models/postit.rb
@@ -1,6 +1,7 @@
 class Postit < ActiveRecord::Base
   validates :name, presence: true
   validates :content, presence: true
+    default_scope { where(published: true) }
   belongs_to :effer
   belongs_to :subeffit
 end
--- app/models/subeffit.rb
+++ app/models/subeffit.rb
@@ -1,6 +1,5 @@
 class Subeffit < ActiveRecord::Base
   validates :name, presence: true
   validates :description, presence: true
-  default_scope { where(published: true) }
   has_many :postits
 end
